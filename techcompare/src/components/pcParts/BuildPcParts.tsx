9fb716dae119c7c4a1bbce3e46d083bae98b2c1b
rsarellano <rjsarellano@gmail.com>
2025-04-08 21:01:13 +0800

    added onclick for selected parts

--- techcompare/src/components/pcParts/BuildPcParts.tsx
+++ techcompare/src/components/pcParts/BuildPcParts.tsx
@@ -6,9 +6,22 @@ import "../../index.css";
 const BuildPcParts = () => {
   const [showParts, isShowParts] = useState(false);
 
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
   const handleClickShowParts = () => {
     isShowParts(true);
   };
+
+  const processors = [
+    { name: "Processor 1", id: 1 },
+    { name: "Processor 2", id: 2 },
+    { name: "Processor 3", id: 3 },
+    { name: "Processor 4", id: 4 },
+    { name: "Processor 6", id: 5 },
+  ];
 
   const fetchPCParts = async () => {
     try {
@@ -27,7 +40,33 @@ const BuildPcParts = () => {
         <button className="add-pc-parts-button">Select Part</button>
       </div>
       <div>
-        <button className="comic-button">Processor</button>
+        <button className="comic-button" onClick={toggleDropdown}>
+          Processor
+        </button>
+      </div>
+
+      {/* dropdown test */}
+
+      <div>
+        <div className="relative inline-block">
+          {isOpen && (
+            <div className="absolute left-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10">
+              <div className="py-1">
+                {processors.map((processor) => (
+                  <div
+                    key={processor.id}
+                    className="px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer"
+                    onClick={() => {
+                      setIsOpen(false);
+                    }}
+                  >
+                    {processor.name}
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
