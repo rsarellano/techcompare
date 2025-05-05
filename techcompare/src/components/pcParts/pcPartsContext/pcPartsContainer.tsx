8eb2ba316723616d9149cb29e02bb6cbc28c00f2
rsarellano <rjsarellano@gmail.com>
2025-05-05 22:44:15 +0800

    added pcPartsContainer

---
+++ techcompare/src/components/pcParts/pcPartsContext/pcPartsContainer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { PcPartsProvider } from "./pcPartsContext";
+import ProcessorOptions from "../pcPartsOptions/processorOptions";
+import DisplayedSelectedPcParts from "../displayedSelectedPcParts/displayedSelectedPcParts";
+
+const pcPartsContainer = () => {
+  return;
+  <PcPartsProvider>
+    <div className="flex flex-col gap-4">
+      <ProcessorOptions />
+      <DisplayedSelectedPcParts />
+    </div>
+  </PcPartsProvider>;
+};
+
+export default pcPartsContainer;
