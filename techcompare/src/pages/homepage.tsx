7c3b4e1467199fefd986eab3027d9446f774554b
rsarellano <rjsarellano@gmail.com>
2025-06-17 09:56:21 +0800

    modification on homepage

--- techcompare/src/pages/homepage.tsx
+++ techcompare/src/pages/homepage.tsx
@@ -4,10 +4,14 @@ import DisplayParts from "../components/pcParts/displayParts";
 const homepage: React.FC = () => {
   return (
     <div>
+      <div className="container w-3">
       homepage
-      <div>
+      <div  className="w-[600px] h-[700px]">
         <DisplayParts />
       </div>
+
+      </div>
+     
     </div>
   );
 };
